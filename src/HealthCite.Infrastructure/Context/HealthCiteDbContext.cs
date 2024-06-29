461c9f1016e98d9930e3065b5339766e525a94a7
Jose Eduardo Del Orbe Minaya <jedelorbeminaya@outlook.com>
2024-06-28 23:46:26 -0400

    Development-Scenary-Two
    28-08-2024

---
+++ src/HealthCite.Infrastructure/Context/HealthCiteDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HealthCite.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthCite.Infrastructure
+{
+    public class HealthCiteDbContext : DbContext
+    {
+        public HealthCiteDbContext(DbContextOptions<HealthCiteDbContext> options) : base(options) { }
+
+        public DbSet<Citas> Citas { get; set; }
+        public DbSet<Consultorios> Consultorios { get; set; }
+        public DbSet<Doctores> Doctores { get; set; }
+        public DbSet<Especialidades> Especialidades { get; set; }
+        public DbSet<EstadoCitas> EstadosCitas { get; set; }
+        public DbSet<Generos> Generos { get; set; }
+        public DbSet<HistorialMedico> HistoriaMedico { get; set; }
+        public DbSet<Pacientes> Pacientes { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //}
+    }
+}
